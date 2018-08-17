34dbf312762d2e665100ca05a2a96e196067fedb
Temitope Olotin <temitope.olotin@andela.com>
2018-08-17 21:01:21 +0100

    add title on image

--- js/main.js
+++ js/main.js
@@ -162,6 +162,7 @@ createRestaurantHTML = (restaurant) => {
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   image.alt = restaurant.name;
+  image.title = restaurant.name;
   li.append(image);
 
   const name = document.createElement('h1');
